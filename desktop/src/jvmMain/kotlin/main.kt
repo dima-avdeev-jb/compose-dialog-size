2a097240b959074e51bd34d2701e1b9b5e12f23d
dima.avdeev <dima.avdeev@jetbrains.com>
2023-01-27 20:54:08 +0400

    temp

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,34 +1,85 @@
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.*
 
+class MyDialogState(positionState: MutableState<WindowPosition>, sizeState: MutableState<DpSize>) : DialogState {
+    override var position: WindowPosition by positionState
+    override var size: DpSize by sizeState
+}
+
 fun main() = application {
-    var visible by remember { mutableStateOf(true) }
+    var dialogVisible by remember { mutableStateOf(true) }
+    var position by remember { mutableStateOf(DpOffset(200.dp, 200.dp)) }
+
     Window(onCloseRequest = ::exitApplication) {
-        if (visible) {
+        Button(onClick = {
+            dialogVisible = true
+        }) {
+            Text("Open Dialog")
+        }
+        key(position) {
+            val dialogState: DialogState =
+                rememberDialogState(WindowPosition(position.x, position.y), DpSize(400.dp, 400.dp))
             Dialog(
-                "Title",
-                onCloseRequest = {
-                    visible = false
-                },
+                title = "Title",
+                onCloseRequest = { dialogVisible = false },
+                state = dialogState,
+                focusable = true,
+                visible = dialogVisible,
             ) {
-                Column {
-                    Text("Test")
-                    Button(onClick = {
-                        visible = !visible
-                    }) {
-                        Text("Close")
+                Box(
+                    modifier = Modifier
+                        .size(dialogState.size)
+                        .border(1.dp, Color.Red),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Box(Modifier.size(150.dp)) {
+                        BtnIcon(Alignment.TopCenter, Icons.Default.KeyboardArrowUp) {
+                            position += DpOffset(0.dp, -10.dp)
+                        }
+                        BtnIcon(Alignment.BottomCenter, Icons.Default.KeyboardArrowDown) {
+                            position += DpOffset(0.dp, 10.dp)
+                        }
+                        BtnIcon(Alignment.CenterStart, Icons.Default.KeyboardArrowLeft) {
+                            position += DpOffset(-10.dp, 0.dp)
+                        }
+                        BtnIcon(Alignment.CenterEnd, Icons.Default.KeyboardArrowRight) {
+                            position += DpOffset(10.dp, 0.dp)
+                        }
+                        Text("Move", Modifier.align(Alignment.Center))
+                    }
+                    Column(Modifier.align(Alignment.CenterEnd)) {
+                        Button({
+                            dialogState.size = dialogState.size.copy(width = dialogState.size.width + 10.dp)
+                        }) { Text("+") }
+                        Button({
+                            dialogState.size = dialogState.size.copy(width = dialogState.size.width - 10.dp)
+                        }) { Text("-") }
+                    }
+                    Row(Modifier.align(Alignment.BottomCenter)) {
+                        Button({
+                            dialogState.size = dialogState.size.copy(height = dialogState.size.height + 10.dp)
+                        }) { Text("+") }
+                        Button({
+                            dialogState.size = dialogState.size.copy(height = dialogState.size.height - 10.dp)
+                        }) { Text("-") }
                     }
                 }
             }
@@ -37,25 +88,10 @@ fun main() = application {
 }
 
 @Composable
-fun Dialog(
-    title: String,
-    onCloseRequest: () -> Unit,
-    content: @Composable () -> Unit
-) {
-    Dialog(
-        onCloseRequest = onCloseRequest,
-        state = rememberDialogState(size = DpSize(400.dp, 400.dp), position = WindowPosition(0.dp, 0.dp)),
-        focusable = true,
-        title = title,
-    ) {
-        Box(
-            modifier = Modifier
-                .size(200.dp)
-                .border(1.dp, Color.Red)
-                .padding(8.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            content()
-        }
-    }
+fun BoxScope.BtnIcon(align: Alignment, icon: ImageVector, onClick: () -> Unit) {
+    IconButton(modifier = Modifier.align(align),
+        content = { Icon(icon, null) },
+        onClick = {
+
+        })
 }
